eb1a27bd5c7aa7b1570f0eb4dfb0ad2cab6cf4f4
na0-S <nayeongsong303@gmail.com>
2026-03-21 03:34:36 +0900

    리액트 스터디 2주차 과제 제출
    Ch.04, 05, 06

---
+++ my-app/src/chapter_05/CommentList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Comment from "./Comment";
+
+const comments = [
+  {
+    name: "송나영",
+    comment: "안녕하세요, 송나영입니다.",
+  },
+  {
+    name: "nayoung",
+    comment: "리액트 재미있어요~!",
+  },
+  {
+    name: "나영06",
+    comment: "저도 리액트 배워보고 싶어요!!",
+  },
+
+
+
+];
+
+function CommentList(props) {
+  return (
+    <div>
+      {comments.map((comment)=> {
+        return (
+          <Comment name={comment.name} comment={comment.comment} />
+        );
+      })}
+    </div>
+  );
+}
+
+export default CommentList;
