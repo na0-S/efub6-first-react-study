a7ed97c0e0f5e3419ca02e6701537d1663c237da
na0-S <nayeongsong303@gmail.com>
2026-04-13 09:32:42 +0900

    [송나영] 리액트 5주차 과제

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,12 +5,15 @@ import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from "./chapter_11/SignUp";
+import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <ProfileCard />
   </React.StrictMode>,
 );
 
